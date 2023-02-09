57c4b1502bf8da9b47d4297d557da5b89e6fe9be
Peter Vang <vang_peter06@yahoo.com>
2023-02-09 15:09:19 -0800

    add header imformation about me

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>
-      Header
+   <header>
+    <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Peter Vang</h1>
+      <h5 className='text-light'>Junior Fullstack Developer</h5>
+      <CTA />
+      <HeaderSocials />
+
+      <div className='me'>
+        {/* include my img later */}
+        <img src={ME} alt="me" />
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
     </div>
+   </header>
   )
 }
 
