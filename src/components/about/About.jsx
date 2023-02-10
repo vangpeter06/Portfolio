577326a603cedada61bc125d765f0020833f6578
Peter Vang <vang_peter06@yahoo.com>
2023-02-10 10:43:51 -0800

    add icon and a brief about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import './about.css';
+import React from "react";
+import "./about.css";
+import ME from '../../assets/me-about.jpg';
+import {BsAward} from 'react-icons/bs';
+import {AiFillFolderOpen} from 'react-icons/ai';
+
 
 const About = () => {
   return (
-    <div>
-      About
+  <section id="about">
+    <h5>Get To Know</h5>
+    <h2>About Me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me image">
+          {/* change img later */}
+          <img src={ME} alt="About image" />
+        </div>
+      </div>
+      <div className="about__content">
+        <div className="about__cards">
+          <article className="about__card">
+            <BsAward className="about__icon" />
+            <h5>Experience</h5>
+            <small>Experience worker</small>
+          </article>
+          <article className="about__card">
+            <AiFillFolderOpen className="about__icon" />
+            <h5>Projects</h5>
+            <small>60+ projects</small>
+          </article>
+        </div>
+        <p>
+          I am a experience software developer willing to leaverage everything I learn to problem solving and being creative. Also a well round in learning and be part of the team.
+        </p>
+        <a href="#contact" className="btn btn-primary">Let's talk</a>
+      </div>
     </div>
+  </section>
   )
-}
+};
 
-export default About
-
+export default About;
