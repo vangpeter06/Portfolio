1f9af4205621523ec322b15251e8a7195336e91b
Peter Vang <vang_peter06@yahoo.com>
2023-02-09 16:38:39 -0800

    add icon home, about, experience, contact, service

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './nav.css';
+import {BiHomeAlt} from 'react-icons/bi';
+import {AiOutlineUser} from 'react-icons/ai';
+import {AiOutlineBook} from 'react-icons/ai';
+import {RiServiceLine} from 'react-icons/ri';
+import {BiMessageRoundedDetail} from 'react-icons/bi';
 
 const Nav = () => {
   return (
     <div>
-      Nav
+      <a href="#"><BiHomeAlt /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><AiOutlineBook /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#contact"><BiMessageRoundedDetail /></a>
+
     </div>
   )
 }
