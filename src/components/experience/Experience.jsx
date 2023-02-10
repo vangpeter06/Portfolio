7decfc8fa867892e956fe17df25ef472bf845d18
Peter Vang <vang_peter06@yahoo.com>
2023-02-10 11:44:05 -0800

    add experience information and react icon

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,85 @@
 import React from 'react';
 import './experience.css';
+import {HiOutlineBadgeCheck} from 'react-icons/hi';
 
 const Experience = () => {
   return (
-    <div>
-      Experience
-    </div>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>HTML</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>jQuery</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>React</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>Node.js</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>C#</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>MySQL</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>Ruby</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>FireStone</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <HiOutlineBadgeCheck />
+              <h4>Git</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+          </div>
+      </div>
+    </section>
   )
 }
 
