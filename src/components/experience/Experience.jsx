ffd2dc98b9143fef3d11c36d63712202ac97bc44
Peter Vang <vang_peter06@yahoo.com>
2023-02-10 17:34:39 -0800

    add div to each header

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -12,34 +12,46 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className='experience__content'>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>HTML</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>CSS</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>JavaScript</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>jQuery</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>React</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>Bootstrap</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
           </div>
         </div>
@@ -47,34 +59,46 @@ const Experience = () => {
         <h3>Backend Development</h3>
           <div className='experience__content'>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>Node.js</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>C#</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>MySQL</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>Ruby</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>FireStone</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <HiOutlineBadgeCheck />
+              <HiOutlineBadgeCheck  className='experience__details-icon' />
+              <div>
               <h4>Git</h4>
               <small className='text-light'>Intermediate</small>
+              </div>
             </article>
           </div>
           </div>
