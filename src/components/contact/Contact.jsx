ccb4b1591370a5e25ec83e1d964dd598de4d1f7a
Peter Vang <vang_peter06@yahoo.com>
2023-02-13 16:35:37 -0800

    add input form and clickable to email in contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './contact.css';
+import {AiOutlineMail} from 'react-icons/ai';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail />
+            <h4>Email</h4>
+            <h5>vang_peter06@yahoo.com</h5>
+            <a href="mailto:vang_peter06@yahoo.com" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* End of contact option */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'></button>
+        </form>
+      </div>
+    </section>
   )
 }
 
