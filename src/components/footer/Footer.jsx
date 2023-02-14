cd10a0572b7e1d4ab7aa7c6a539c69ff41ad18a8
Peter Vang <vang_peter06@yahoo.com>
2023-02-13 19:42:25 -0800

    add content to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import './footer.css';
+import {BsLinkedin} from 'react-icons/bs';
+import {FiGithub} from 'react-icons/fi';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Peter Vang</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+      <a href="https://www.linkedin.com/in/peter-vang-portland/" target="__blank"><BsLinkedin /></a>
+      <a href="https://github.com/vangpeter06" target="__blank"><FiGithub /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Peter Vang. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
